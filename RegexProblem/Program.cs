4a16d91606d88814e034bf07a5ba16938a63eca1
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-08-03 17:28:58 +0530

    [RK Nanda] Adding regex for firstName

--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -3,7 +3,22 @@ class Program
 {
     static void Main(string[] args)
     {
+        string fname;
+        RegExpression rg = new RegExpression();
         Console.WriteLine("Welcome to Regular Expression Problems");
+        Console.WriteLine("Enter your first name");
+        fname = Console.ReadLine();
+
+        if(rg.ValidateFirstName(fname))
+        {
+            Console.WriteLine($"The name {fname} is Valid");
+        }
+        else
+        {
+            Console.WriteLine($"The name {fname} is InValid");
+        }
+        Console.ReadLine(); 
+
     }
 }
 
---
+++ RegexProblem/RegExpression.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegexProblem
+{
+	public class RegExpression
+    {
+	
+		public static string REGEX_FIRSTNAME = "^[A-Z][a-zA-Z]{2,}$";
+		public bool ValidateFirstName(string fname)
+		{
+			return Regex.IsMatch(fname, REGEX_FIRSTNAME);
+		}
+	}
+}
+
